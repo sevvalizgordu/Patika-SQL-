9e67abf65602731db43be901bffa0bcc3f8b2625
sevvalizgordu <156258179+sevvalizgordu@users.noreply.github.com>
2024-02-18 15:17:39 +0300

    Patika SQL Ödev 3

---
+++ ödev 3.sql
@@ -0,0 +1,4 @@
+--SELECT country from country WHERE country LIKE 'A%a';
+--SELECT country FROM country WHERE country LIKE '_____%n'
+--SELECT title FROM film WHERE title ILIKE '%t%t%t%t%'
+SELECT * FROM film WHERE length > 90 AND rental_rate = 2.99 AND title LIKE 'C%';

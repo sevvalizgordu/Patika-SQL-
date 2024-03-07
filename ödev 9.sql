6f7a7cd6b2129fda124e79e2801ccd1ebfed6a5f
sevvalizgordu <156258179+sevvalizgordu@users.noreply.github.com>
2024-03-07 16:57:50 +0300

    Patika SQL Ödev 9

---
+++ ödev 9.sql
@@ -0,0 +1,8 @@
+-- SELECT city, country FROM city 
+-- JOIN country ON city.country_id = country.country_id;
+
+--SELECT payment_id, first_name, last_name FROM payment
+--JOIN customer ON customer.customer_id = payment.customer_id;
+
+SELECT rental_id, first_name, last_name FROM rental
+JOIN customer ON customer.customer_id = rental.customer_id;

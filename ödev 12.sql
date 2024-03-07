6df683f38c72befa0f74c480d9594cc3a58f88a8
sevvalizgordu <156258179+sevvalizgordu@users.noreply.github.com>
2024-03-07 22:17:45 +0300

    Patika SQL Ödev 12

---
+++ ödev 12.sql
@@ -0,0 +1,15 @@
+SELECT COUNT(*) FROM film
+WHERE length >
+(SELECT AVG(length) FROM film);
+
+SELECT COUNT(*) FROM film
+WHERE rental_rate =
+(SELECT MAX(rental_rate) FROM film);
+
+SELECT rental_rate, replacement_cost FROM film
+WHERE rental_rate =
+(SELECT MIN(rental_rate) FROM film) 
+AND replacement_cost =
+(SELECT MIN(replacement_cost) FROM film);
+
+SELECT COUNT(customer_id) FROM payment GROUP BY customer_id ORDER BY COUNT(customer_id) DESC;
